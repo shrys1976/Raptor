7d95b374f14eac314c61635e2c4fc39e645c3aba
shrys <shreyasjain2018@gmail.com>
2026-04-04 01:10:53 +0530

    wrap ops functions with Module

--- microtorch/nn.py
+++ microtorch/nn.py
@@ -40,5 +40,42 @@ class Module:
         return self.forward(*inputs)                            
 
 
+
+class Linear(Module):
+    def __init__(self, in_features, out_features, bias =True):
+        limit  = np.sqrt(6.0/in_features)
+
+        self.weight = Tensor(
+            np.random.uniform(-limit, limit, (out_features, in_features)).astype(np.float32)
+
+        )
+
+        self.bias =( Tensor(np.zeros(out_features, dtype = np.float32))
+        if bias else None
+
+        )        
+
+    def forward(self,x):
+        out - x @ self.weight.transpose()
+
+
+        if self.bias is not None:
+            out = out + self.bias
+
+        return out       
+
+
+
+class ReLU(Module):
+    def forward(self,x):
+        return ops.relu(x)
+
         
-                                                                
+class Sigmoid(Module):
+    def forward(self, x):
+        return ops.sigmoid(x)
+
+
+class Tanh(Module):
+    def forward(self, x):
+        return ops.tanh(x)
